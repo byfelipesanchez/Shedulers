543570c87bf197fce3ecdb1439d23cb57a89c495
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-01-30 23:24:45 -0300

    Add files via upload

---
+++ scheduler.py
@@ -0,0 +1,7 @@
+import webbrowser
+import subprocess
+
+webbrowser.open('https://moodle.cmc.com.br/my/')
+webbrowser.open('https://calendar.google.com/calendar/u/0/r')
+subprocess.call('C:/Users/2018f/AppData/Local/Microsoft/Teams/Update.exe --processStart "Teams.exe"')
+# subprocess.call('C:/Users/2018f/AppData/Local/Discord/Update.exe --processStart Discord.exe')
