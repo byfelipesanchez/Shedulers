434bd4ccf2d850ba7406bd8490df01bb3871b3a8
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-02-14 13:51:18 -0300

    send email( )
    the send email scrpit (diff script) works fine, it sends emails when ran. however, i cant seem to figure out how to implemet that into this webscraper

--- WebScraping/main.py
+++ WebScraping/main.py
@@ -4,52 +4,61 @@ import bs4
 import gspread
 from bs4 import BeautifulSoup
 import datetime
-import smtplib
+import smtplib, ssl
 
 
+def send_email():
+    port = 465
 
-#
-# sh.append_row('first', 'second')
+    sender, password = 'senderemailfs@gmail.com', 'FelipeSanchezsSenderEmail'
 
+    recieve = '2018.felipe.sanchez@gmail.com'
+
+    message = '''\
+    Subject: Testing Python Email
+
+    Message via Python!
+
+    Felipe
+
+    '''
+
+    context = ssl.create_default_context()
+
+    print("starting to send")
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context)as server:
+        server.login(sender, password)
+        server.sendmail(sender, recieve, message)
+
+    print("email sent!")
 
 def request():
     r = requests.get('https://br.financas.yahoo.com/quote/KLBN4.SA?p=KLBN4.SA&.tsrc=fin-srch')
     soup = bs4.BeautifulSoup(r.text, 'lxml')
     return soup
 
-
-def parse(soup):
-    date = datetime.datetime.now()
-    name = soup.find('h1', class_ = 'D(ib) Fz(18px)').text.strip()
-    price = soup.find_all('div', {'class':"My(6px) Pos(r) smartphone_Mt(6px)"})[0].find('span').text
-    total = {'date':date, 'name': name ,'price':price}
-    return total
-
-def output(total):
-    gc = gspread.service_account(filename='creds.json')
-    sh = gc.open('webscraper').sheet1
-    sh.append_row([str(total['date']), str(total['name']), str(total['price'])])
+class script:
 
 
-data = request()
-total = parse(data)
-output(total)
-# print(total)
+    def parse(soup):
+        date = datetime.datetime.now()
+        name = soup.find('h1', class_ = 'D(ib) Fz(18px)').text.strip()
+        price = soup.find_all('div', {'class':"My(6px) Pos(r) smartphone_Mt(6px)"})[0].find('span').text
+        total = {'date':date, 'name': name ,'price':price}
+        return total
 
 
-# sender_email = "2018.felipe.sanchez@gmail.com"
-# rec_email = "2018.felipe.sanchez@gmail.com"
-# password = input("enter password")
-# message = "test"
-#
-#
-# server = smtplib.SMTP('smtp.gmail.com', 587)
-# server.starttls()
-# server.login(sender_email, password)
-# server.sendmail(sender_email, rec_email, message)
-# print("email sent", rec_email)
+    def output(total):
+        gc = gspread.service_account(filename='creds.json')
+        sh = gc.open('webscraper').sheet1
+        sh.append_row([str(total['date']), str(total['name']), str(total['price'])])
 
-#TODO: webscraping of other stocks and sending email when stock reaches a certain price
+    data = request()
+    total = parse(data)
+    output(total)
+    print(total)
 
-# while True:
-#     print('the current price is: ' + str(parsePrice()))
+
+
+
+
