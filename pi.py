2ff8b052f5a8812c58c881ea8966431c36554de1
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-06-09 09:40:06 -0300

    Update pi.py

--- pi.py
+++ pi.py
@@ -17,3 +17,23 @@ def pi(n):
     
     
     
+class Random:
+  def __init__(self, pts_circle, pts_total, distance):
+    self.pts_circle = pts_circle
+    self.pts_total = pts_total 
+    self.distance = distance
+    
+ def estimate_pi(n, x, y):
+  self.x = x
+  self.y = y
+  self.pts_circle = 0
+  self.pts_total = 0
+  for _ in range(n):
+    self.x = random.uniform(0,1)
+    self.y = random.uniform(0,1)
+    self.distance = self.x**2 + self.y**2
+    if self.distanc <= 1:
+      self.pts_circle += 1
+    pts_total += 1
+
+return 4 + pts_circle/pts_total
